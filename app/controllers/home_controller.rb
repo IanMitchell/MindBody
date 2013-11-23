6f06ff2463df1c7e5cadb11331e2e27e1cedb986
Ian Mitchell <ian.mitchel1@live.com>
2013-11-23 13:18:03 -0800

    Fixed Results Duplication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,27 @@
 class HomeController < ApplicationController
   def index
     if params[:query].present?
+      @results = Array.new
+
       @locations = Location.search params[:query], partial: true
       @tags = Tag.search params[:query], partial: true
+
+      @locations.each do |loc|
+        @results << loc
+      end
+
+      add = true
+      @tags.each do |tag|
+        tag.locations.each do |tag_loc|
+          @results.each do |loc|
+            add = false if loc.name == tag_loc.name
+          end
+          @results << tag_loc if add
+          add = true
+        end
+      end
     else
-      @locations = Location.all
+      @results = Location.all
     end
   end
 
