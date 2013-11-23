9b9788db6a81a94b861ccdfeec788bafb2759777
Ian Mitchell <ian.mitchel1@live.com>
2013-11-22 18:39:47 -0800

    Added Tag Validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :locations
+
+  validates :name,  :presence => true,
+                    :uniqueness => true,
+                    :length => { :minimum => 1, :maximum => 75 }
 end
