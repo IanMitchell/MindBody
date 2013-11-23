2831a59440b3848e891fbf0bb76d8701b301e369
Ian Mitchell <ian.mitchel1@live.com>
2013-11-22 17:47:18 -0800

    Initial Commit
    MindBody Hackathon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MindBody::Application.config.secret_key_base = '4d6d767209fcded9a7af6e33a61abab5e5cbd4f714fe2b8e003ba057845d164a6c8883c0dd0692c47e6fe42377030147a7deef47692fa2bea87b676755f2c2f2'
