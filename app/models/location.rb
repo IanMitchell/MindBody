3dc5e0286322386fff0fd4dc1dee2b1b11bbb57f
Ian Mitchell <ian.mitchel1@live.com>
2013-11-22 18:40:41 -0800

    Added Location Validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,10 @@
 class Location < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :tags
+
+  validates :name,  :presence => true,
+                    :length => { :minimum => 3, :maximum => 75 }
+
+  validates :url,   :format => { :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix },
+                    :allow_blank => true
 end
